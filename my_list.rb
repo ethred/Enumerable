8742c20e0d7c09b995b057536581db533052935b
Bett Enock <bettenock968@gmail.com>
2023-10-04 16:42:31 -0700

    Initialize items

--- my_list.rb
+++ my_list.rb
@@ -1,9 +1,12 @@
 require_relative 'my_enumerable'
 class MyList
   include MyEnumerable
+  #INITIALIZE ITEMS
+  def initialize(*items)
+    @list = items
+  end
 
   def each(&block)
     @list.each(&block)
   end
-  
 end
