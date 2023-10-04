56344cdc424789605cf669b2df702ec537f594e3
Bett Enock <bettenock968@gmail.com>
2023-10-04 16:38:24 -0700

    create block method

--- my_list.rb
+++ my_list.rb
@@ -2,11 +2,8 @@ require_relative 'my_enumerable'
 class MyList
   include MyEnumerable
 
-  def initialize(*items)
-    @list = items
-  end
-
   def each(&block)
     @list.each(&block)
   end
+  
 end
